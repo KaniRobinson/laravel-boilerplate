9b7851f04e9771f3e532a09bf3c9c8369ce8ac54
KaniRobinson <me@kanirobinson.co.uk>
2025-11-14 21:52:07 +0000

    Updated Gate Providers to allow my email address

--- app/Providers/PulseServiceProvider.php
+++ app/Providers/PulseServiceProvider.php
@@ -3,8 +3,10 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+use App\Models\User;
 
-class AppServiceProvider extends ServiceProvider
+class PulseServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
@@ -19,6 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // 
+        Gate::define('viewPulse', function ($user = null) {
+            return in_array(optional($user)->email, [
+                'me@kanirobinson.co.uk',
+            ]);
+        });
     }
 }
